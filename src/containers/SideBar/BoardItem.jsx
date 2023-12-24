d80719e9aedae702b563a27525c7ebf4962b76bb
RusithHansana <97432521+RusithHansana@users.noreply.github.com>
2023-12-25 01:15:49 +0530

    sidebar items dynamical color changes

---
+++ src/containers/SideBar/BoardItem.jsx
@@ -0,0 +1,39 @@
+import {React, useState} from "react";
+
+import './BoardItem.scss';
+
+const BoardItem = ({item}) => {
+    const [isHovered, setHovered] = useState(false);
+
+    const handleMouseEnter = () => {
+        setHovered(true);
+    };
+
+    const handleMouseLeave = () => {
+        setHovered(false);
+    };
+
+    const hoverStyles = {
+        background: isHovered ? `var(${item.color})` : 'inherit',
+        color: isHovered ? 'var(--secondary-color)' : 'inherit',
+        borderRadius: isHovered ? '10px' : 'inherit',
+        padding: isHovered ? '1rem' : 'inherit',
+
+    };
+
+    return (
+        <div className="board-items" style={{borderLeft: `4px solid var(${item.color})`}}>
+            <button>
+                <li
+                    onMouseEnter={handleMouseEnter}
+                    onMouseLeave={handleMouseLeave}
+                    style={hoverStyles}
+                >
+                    {item.name}
+                </li>
+            </button>
+        </div>
+    );
+}
+
+export default BoardItem;
--- src/containers/SideBar/Sidebar.jsx
+++ src/containers/SideBar/Sidebar.jsx
@@ -1,17 +1,41 @@
 import React from "react";
 
+import BoardItem from "./BoardItem.jsx";
 import './Sidebar.scss';
+
+const boards = [
+    { id: 1, name: 'Website Redesign', color: '--highlight-green' },
+    { id: 2, name: 'Mobile App', color: '--highlight-cyan' },
+    { id: 3, name: 'Blog Theme', color: '--highlight-pink' },
+    { id: 4, name: 'UI Kit', color: '--highlight-purple' },
+    { id: 5, name: 'Icon Set', color: '--highlight-blue' },
+    { id: 6, name: 'Marketing Site', color: '--highlight-green' },
+    { id: 7, name: 'Dashboard UI', color: '--highlight-cyan' },
+    { id: 8, name: 'API Integration', color: '--highlight-pink' },
+    { id: 9, name: 'Calendar Widget', color: '--highlight-purple' },
+    { id: 10, name: 'Chat Application', color: '--highlight-blue' },
+    { id: 11, name: 'Project Management', color: '--highlight-yellow' },
+    { id: 12, name: 'E-commerce Platform', color: '--highlight-cyan' },
+    { id: 13, name: 'Blog Redesign', color: '--highlight-pink' },
+    { id: 14, name: 'Dashboard Analytics', color: '--highlight-purple' },
+    { id: 15, name: 'Social Media Integration', color: '--highlight-blue' },
+    { id: 16, name: 'Task Manager', color: '--highlight-green' },
+    { id: 17, name: 'Event Calendar', color: '--highlight-cyan' },
+    { id: 18, name: 'Customer Support Portal', color: '--highlight-pink' },
+    { id: 19, name: 'Knowledge Base', color: '--highlight-purple' },
+    { id: 20, name: 'Collaboration Tool', color: '--highlight-blue' },
+  ];
 
 const Sidebar = () => {
     return (
         <div className="app__sidebar">
             <ul className="app__sidebar-boards">
-                <div className="board-items">
-                    <button><li>Happening App</li></button>
-                </div>
-                <div className="board-items">
-                    <button><li>Portfolio</li></button>
-                </div>
+                {
+                    boards.map(item => (
+                            <BoardItem item={item} key={item.id}/>
+                        )
+                    )
+                }
             </ul>
         </div>
     );
