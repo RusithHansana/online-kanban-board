f91dccd6eb30bc85849fbb6b9859392178cd30f0
RusithHansana <97432521+RusithHansana@users.noreply.github.com>
2023-12-26 17:04:48 +0530

    adding media queries

--- src/containers/SideBar/Sidebar.jsx
+++ src/containers/SideBar/Sidebar.jsx
@@ -1,22 +1,36 @@
-import React from "react";
+import React, { useState } from 'react';
+import { HiMenuAlt2, HiX } from 'react-icons/hi';
+import { motion } from 'framer-motion';
 
 import { Boards } from "../../utils/BoardData/Boards.js";
 import './Sidebar.scss';
 import BoardItem from "./BoardItem.jsx";
 
 const Sidebar = () => {
+    const [ toggle, setToggle ] = useState(false);
+
     return (
         <div className="app__sidebar">
-            <h1 className="app__sidebar-title">Your Boards</h1>
-            <div className="app__sidebar-scroll">
-                <ul className="boards">
-                    {
-                        Boards.map(item => (
-                                <BoardItem item={item} key={item.id} />
+            <div className="app__sidebar-bar">
+                <h1 className="app__sidebar-title">Your Boards</h1>
+                <div className="app__sidebar-scroll">
+                    <ul className="boards">
+                        {
+                            Boards.map(item => (
+                                    <BoardItem item={item} key={item.id} />
+                                )
                             )
-                        )
-                    }
-                </ul>
+                        }
+                    </ul>
+                </div>
+            </div>
+            <div className="app__sidebar-menu">
+                <HiMenuAlt2 onClick={ () => setToggle(true) }/>
+                {
+                    toggle && (
+                        <h1 className="app__sidebar-menu title">Your Boards</h1>
+                    )
+                }
             </div>
         </div>
     );
