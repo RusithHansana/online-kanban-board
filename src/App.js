26d1bbb5b5c0cffc44e6ff874f340b4d187d2dd2
RusithHansana <97432521+RusithHansana@users.noreply.github.com>
2024-01-13 19:21:11 +0530

    retrieving user from sign up

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { useState } from "react";
 import { DragDropContext } from "react-beautiful-dnd";
+import { useLocation } from "react-router-dom";
 
 import "./App.scss";
-import { Boards, Users } from "./utils/BoardData/Boards.js";
+import { Boards } from "./utils/BoardData/Boards.js";
 import Navbar from "./components/NavBar/Navbar.jsx";
 import Sidebar from "./components/SideBar/Sidebar.jsx";
 import TaskBoard from "./components/TaskBoard/TaskBoard.jsx";
 
 const App = () => {
-  const [user, setUser] = useState(Users.u1);
+  const loc = useLocation();
+  const user = loc.state;
   const [boards, setBoards] = useState(user.boardList);
 
   const onDragEnd = (result) => {};
