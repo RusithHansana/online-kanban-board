f8ed2f627a8ba795b585366250925218fd5f2775
RusithHansana <97432521+RusithHansana@users.noreply.github.com>
2024-01-31 17:42:18 +0530

    boardname update

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -1,14 +1,36 @@
-import React from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { LogOut } from "react-feather";
 
 import './Navbar.scss';
 import { Boards } from "../../utils/BoardData/Boards";
 
 const Navbar = ({ activeBoardId }) => {
+    const inputRef = useRef();
+    const [boardName, setBoardName] = useState('');
+
+    useEffect(() => {
+        setBoardName(Boards[activeBoardId].name);
+    }, [activeBoardId]);
+
+    const handleInputChange = (e) => {
+        //onKeyDown identifies keyboard events
+        if (e.key === 'Enter') {
+            setBoardName(e.target.value);
+            //update the boardname in the backend
+
+
+            //clearing the input field after entering the value
+            inputRef.current.value = '';
+        }
+    }
     return (
         <div className="app__navbar">
             <div className="app__navbar-input">
-                <input type="text" placeholder={Boards[activeBoardId].name} />
+                <input
+                    ref={inputRef}
+                    type="text"
+                    placeholder={boardName}
+                    onKeyDown={handleInputChange} />
             </div>
             <div className="app__navbar-button">
                 <LogOut />
