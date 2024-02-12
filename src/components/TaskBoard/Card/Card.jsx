317ac32e08cec657872de444c9d2f2ac4ccc8a7d
RusithHansana <97432521+RusithHansana@users.noreply.github.com>
2024-02-12 13:38:34 +0530

    css changes in add taksk button

--- src/components/TaskBoard/Card/Card.jsx
+++ src/components/TaskBoard/Card/Card.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect }from 'react'
+import React, { useState, useEffect } from 'react'
 import { Draggable, Droppable } from 'react-beautiful-dnd';
 import { PlusCircle } from 'react-feather'
 
@@ -6,15 +6,15 @@ import Task from '../Task/Task';
 import './Card.scss';
 import { Tasks } from '../../../utils/BoardData/Boards';
 
-const Card = ({ card, index}) => {
-    const [ enabled, setEnabled ] = useState(false);
+const Card = ({ card, index }) => {
+    const [enabled, setEnabled] = useState(false);
 
     useEffect(() => {
         const animation = requestAnimationFrame(() => setEnabled(true));
 
         return () => {
-        cancelAnimationFrame(animation);
-        setEnabled(false);
+            cancelAnimationFrame(animation);
+            setEnabled(false);
         };
     }, []);
 
@@ -28,61 +28,61 @@ const Card = ({ card, index}) => {
             index={index}
             type="card"
         >
-        {(provided) => (
-            <div
-                className="app__card"
-                {...provided.draggableProps}
-                ref={provided.innerRef}
-            >
-                <h1 className="app__card-title"
-                    {...provided.dragHandleProps}
+            {(provided) => (
+                <div
+                    className="app__card"
+                    {...provided.draggableProps}
+                    ref={provided.innerRef}
                 >
-                    {card.title}
-                </h1>
-                <Droppable
-                    droppableId={card.id}
-                    type="task"
-                >
-                    {
-                        (provided) => (
-                            <ul 
-                                className="app__card-tasklist"
-                                {...provided.droppableProps}
-                                ref={provided.innerRef}    
-                            >
-                                {card.taskOrder.map((taskId, index) => (
-                                    <Draggable
-                                        draggableId={taskId}
-                                        index={index}
-                                        type="task"
-                                        key={taskId}
-                                    >
-                                        {
-                                            (provided) => (
-                                                <li 
-                                                    {...provided.draggableProps}
-                                                    {...provided.dragHandleProps}
-                                                    ref={provided.innerRef}
-                                                >
-                                                    <Task task={Tasks[taskId]} index={index}/>
-                                                </li>
-                                            )
-                                        }
-                                    </Draggable>
-                                    
-                                ))}
-                                {provided.placeholder}
-                                {
-                                 card.title === 'To do'?<button className="btn-tasklist"><PlusCircle />Add More</button>: null   
-                                }
-                            </ul>
-                        ) 
-                    }
-                </Droppable>
-            </div>
-        )}
-      </Draggable>
-  )
+                    <h1 className="app__card-title"
+                        {...provided.dragHandleProps}
+                    >
+                        {card.title}
+                    </h1>
+                    <Droppable
+                        droppableId={card.id}
+                        type="task"
+                    >
+                        {
+                            (provided) => (
+                                <ul
+                                    className="app__card-tasklist"
+                                    {...provided.droppableProps}
+                                    ref={provided.innerRef}
+                                >
+                                    {card.taskOrder.map((taskId, index) => (
+                                        <Draggable
+                                            draggableId={taskId}
+                                            index={index}
+                                            type="task"
+                                            key={taskId}
+                                        >
+                                            {
+                                                (provided) => (
+                                                    <li
+                                                        {...provided.draggableProps}
+                                                        {...provided.dragHandleProps}
+                                                        ref={provided.innerRef}
+                                                    >
+                                                        <Task task={Tasks[taskId]} index={index} />
+                                                    </li>
+                                                )
+                                            }
+                                        </Draggable>
+
+                                    ))}
+                                    {provided.placeholder}
+                                    {
+                                        card.title === 'To do' ? <button className="btn-tasklist"><PlusCircle /></button> : null
+                                    }
+                                </ul>
+                            )
+                        }
+                    </Droppable>
+                </div>
+            )}
+        </Draggable>
+    )
 }
 
 export default Card;
