09168275db3fd05cfebcee93c9fa359c74506522
RusithHansana <97432521+RusithHansana@users.noreply.github.com>
2024-03-18 23:54:07 +0530

    Add "Boards" tag type to apiSlice

---
+++ src/slices/boardsApiSlice.js
@@ -0,0 +1,49 @@
+import { apiSlice } from "./apiSlice";
+
+const Boards_URL = "http://localhost:5000/api/boards";
+
+export const boardsApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    getBoards: builder.mutation({
+      query: (data) => ({
+        url: `${Boards_URL}/`,
+        method: "GET",
+        body: data,
+      }),
+    }),
+    add: builder.mutation({
+      query: (data) => ({
+        url: `${Boards_URL}/add`,
+        method: "POST",
+        body: data,
+      }),
+    }),
+    updateBoard: builder.mutation({
+      query: () => ({
+        url: `${Boards_URL}/update`,
+        method: "PUT",
+      }),
+    }),
+    deleteBoard: builder.mutation({
+      query: ({ data }) => ({
+        url: `${Boards_URL}/profile`,
+        method: "DELETE",
+        body: data,
+      }),
+    }),
+    deleteAllBoards: builder.mutation({
+      query: () => ({
+        url: `${Boards_URL}/delete/all`,
+        method: "DELETE",
+      }),
+    }),
+  }),
+});
+
+export const {
+  useGetBoardsMutation,
+  useAddMutation,
+  useUpdateBoardMutation,
+  useDeleteBoardMutation,
+  useDeleteAllBoardsMutation,
+} = boardsApiSlice;
