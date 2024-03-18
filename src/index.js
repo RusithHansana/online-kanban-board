906686f97e6eec17b3a960a4fc3975e9a7775625
RusithHansana <97432521+RusithHansana@users.noreply.github.com>
2024-03-18 19:16:25 +0530

    Add Redux Toolkit and update dependencies

--- src/index.js
+++ src/index.js
@@ -1,29 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { 
-  createBrowserRouter,
-  RouterProvider 
-} from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import './index.css';
-import App from './App';
-import SignUp from './components/SignUp/SignUp';
+import "./index.css";
+import App from "./App";
+import SignUp from "./components/SignUp/SignUp";
+
+import store from "./store.js";
+import { Provider } from "react-redux";
 
 const router = createBrowserRouter([
   {
-    path:'/',
-    element: <SignUp />
+    path: "/",
+    element: <SignUp />,
   },
   {
-    path:'/yourboards',
-    element: <App />
-  }
-])
+    path: "/yourboards",
+    element: <App />,
+  },
+]);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </Provider>
 );
-
